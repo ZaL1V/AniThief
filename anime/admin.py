02d04ecedeea06bdbb3533a03e1d0a674b79a0dc
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-03-08 18:49:52 +0200

    update admin models view

--- anime/admin.py
+++ anime/admin.py
@@ -1,7 +1,74 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Anime, Series, Genre
 
 
-admin.site.register(Anime)
-admin.site.register(Series)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'image',
+        'number_of_series',
+    )
+
+    @staticmethod
+    def number_of_series(instance):
+        return instance.series_set.count()
+
+    def image(self, obj):
+        if not obj.img_url:
+            return '-'
+        return format_html(
+            f"<img style='width: 60px; height: 60px'  loading='lazy'"
+            f"src='{obj.img_url}'>"
+        )
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'preview_',
+        'season',
+        'series_number',
+        'has_video_360',
+        'has_video_480',
+        'has_video_720',
+        'has_video_1080',
+        'anime_link',
+    )
+
+    def preview_(self, obj):
+        if not obj.preview:
+            return '-'
+        return format_html(
+            f"<img style='width: 60px; height: 60px'  loading='lazy'"
+            f"src='{obj.preview}'>"
+        )
+
+    def has_video_360(self, obj):
+        return bool(obj.video_360)
+
+    def has_video_480(self, obj):
+        return bool(obj.video_480)
+
+    def has_video_720(self, obj):
+        return bool(obj.video_720)
+
+    def has_video_1080(self, obj):
+        return bool(obj.video_1080)
+
+    def anime_link(self, obj):
+        return format_html('<a href="{}">{}</a>'.format(
+            reverse("admin:anime_anime_change", args=(obj.anime.pk,)),
+            obj.anime.title
+        ))
+
+    has_video_360.boolean = True
+    has_video_480.boolean = True
+    has_video_720.boolean = True
+    has_video_1080.boolean = True
+
+
+admin.site.register(Anime, AnimeAdmin)
+admin.site.register(Series, SeriesAdmin)
 admin.site.register(Genre)
